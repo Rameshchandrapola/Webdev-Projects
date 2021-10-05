6fb742b008ee5d37856dcb5e236bc657c150b15a
rameshchandra22 <pola.rameshchandra22@gmail.com>
2021-10-05 09:54:53 +0530

    updated dicegame

--- Dice_game/script.js
+++ Dice_game/script.js
@@ -9,17 +9,15 @@ function init(){
 init();
 document.querySelector("img.img1").setAttribute("src",randomDiceGenerator());
 document.querySelector("img.img2").setAttribute("src",randomDiceGenerator());
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
 
 // if dice1 < dice2
 if(document.querySelector("img.img1").getAttribute("src") < document.querySelector("img.img2").getAttribute("src")) {
-    document.querySelector("h1").innerHTML="Player 2 Won";
+    document.getElementById("winner").textContent = sessionStorage.getItem("player2")+" Won🎉";
 }
 
 // if dice1 > dice2
 else if (document.querySelector("img.img1").getAttribute("src") > document.querySelector("img.img2").getAttribute("src")) {
-    document.querySelector("h1").innerHTML=" Player 1 Won";
+    document.getElementById("winner").textContent = sessionStorage.getItem("player1")+" Won🎉";
 }
 
 else {
